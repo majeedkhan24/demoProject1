697d0dce51fac6976167bbe55d41d85bc17585e9
majeedkhan <majeedkhan24@gmail.com>
2021-02-25 20:53:24 -0600

    second commit

---
+++ src/test/java/com/AutomationTasks/demoProject1/TestCase1.java
@@ -0,0 +1,64 @@
+package com.AutomationTasks.demoProject1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class TestCase1 {
+	
+	public static WebDriver driver;
+@BeforeMethod
+	public void launchDriver()
+	{
+		System.setProperty("webdriver.geckodriver.driver","E:\\Softwares\\Computer softwares\\Drivers\\geckodriver.exe");
+		
+
+        //Setting webdriver.gecko.driver property
+        System.setProperty("webdriver.gecko.driver","E:\\Softwares\\Computer softwares\\Drivers\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		
+	}
+	@Test
+	public void Test1() {
+		
+		driver.navigate().to("https://automationtalks.com/");
+		System.out.println("Test1 title is :"+driver.getTitle());
+		
+	}
+	@Test
+	public void Test12() {
+		
+		driver.navigate().to("https://automationtalks.com/");
+		System.out.println("Test2 title is :"+driver.getTitle());
+		
+		
+	}
+	@Test
+	public void Test3() {
+		
+		driver.navigate().to("https://automationtalks.com/");
+		System.out.println("Test3 title is :"+driver.getTitle());
+		
+		
+	}
+
+@AfterMethod
+public void quit()
+{
+	driver.quit();
+}
+
+
+
+
+
+
+}
